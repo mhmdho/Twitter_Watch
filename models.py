8a189377945dfae410ec54c88509822dcc55f328
Mohammad Hadiomid <hadiomidm@gmail.com>
2023-03-09 15:54:38 +0000

    Create models.py and define database tables

---
+++ models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base, engine
+
+
+class Accounts(Base):
+  __tablename__ = "Accounts"
+
+  id = Column(Integer, primary_key=True, index=True)
+  username = Column(String)
+  description = Column(String)
+
+
+class Tweets(Base):
+  __tablename__ = "Tweets"
+
+  id = Column(Integer, primary_key=True, index=True)
+  date = Column(DateTime)
+  username = Column(String)
+  content = Column(String)
+
+
+class Replies(Base):
+  __tablename__ = "Relplies"
+
+  id = Column(Integer, primary_key=True, index=True)
+  date = Column(DateTime)
+  username = Column(String)
+  content = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
