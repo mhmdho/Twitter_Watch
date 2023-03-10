f2027983069480df539466eb58bfa11eefd43ae9
Mohammad Hadiomid <hadiomidm@gmail.com>
2023-03-10 19:40:54 +0000

    Define audience API to get active audience of each account

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from fastapi_utils.tasks import repeat_every
 from database import SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from scrapers import get_replies, get_tweets, get_accounts
 import uvicorn
 import models
@@ -23,12 +24,43 @@ def get_db():
     db.close()
 
 
+
 @app.on_event("startup")
 @repeat_every(seconds=10)
-def tweets_task():
-  for item in accounts:
-    get_tweets(item)
-    get_replies(item)
+def tweets_task0():
+    get_tweets(accounts[0])
+    get_replies(accounts[0])
+
+@app.on_event("startup")
+@repeat_every(seconds=11)
+def tweets_task1():
+    get_tweets(accounts[1])
+    get_replies(accounts[1])
+
+@app.on_event("startup")
+@repeat_every(seconds=12)
+def tweets_task2():
+    get_tweets(accounts[2])
+    get_replies(accounts[2])
+
+@app.on_event("startup")
+@repeat_every(seconds=13)
+def tweets_task3():
+    get_tweets(accounts[3])
+    get_replies(accounts[3])
+
+@app.on_event("startup")
+@repeat_every(seconds=14)
+def tweets_task4():
+    get_tweets(accounts[4])
+    get_replies(accounts[4])
+
+@app.on_event("startup")
+@repeat_every(seconds=15)
+def tweets_task5():
+    get_tweets(accounts[5])
+    get_replies(accounts[5])
+
 
 
 
@@ -48,5 +80,17 @@ def replies_api(account: str, db: Session = Depends(get_db)):
                   models.Accounts.username == account.lower()).first()
   return the_account.acreplies
 
-
+@app.get("/api/v1/{account}/audience")
+def audience_api(account: str, db: Session = Depends(get_db)):
+  the_account = (db.query(models.Accounts)
+                   .filter(models.Accounts.username == account.lower())
+                   .first())
+  result = (db.query(models.Replies.username, func.count(models.Replies.username))
+               .having(models.Replies.tweet_owner_id == the_account.id)
+               .group_by(models.Replies.username)
+               .order_by(func.count(models.Replies.username).desc())
+               .limit(10)
+           )
+  return result
+  
 uvicorn.run(app, port = 8080, host = "0.0.0.0")
