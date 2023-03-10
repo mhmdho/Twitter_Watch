41234016792c37551d04ddf2034437afdd7be9cd
Mohammad Hadiomid <hadiomidm@gmail.com>
2023-03-10 11:05:18 +0000

    Pass specific twitter accounts to get_accounts and define related api

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI, Depends
 from fastapi_utils.tasks import repeat_every
 from database import SessionLocal
 from sqlalchemy.orm import Session
-from scrapers import get_replies, get_tweets
+from scrapers import get_replies, get_tweets, get_accounts
 import uvicorn
 import models
+
+
+accounts = ['@alikarimi_ak8', '@elonmusk', '@BarackObama',
+            '@taylorlorenz', '@cathiedwood', '@ylecun']
+get_accounts(accounts)
 
 
 app = FastAPI()
@@ -22,12 +27,12 @@ def get_db():
 @repeat_every(seconds=10)
 def background_task():
   get_tweets('barackobama')
-  # get_replies('barackobama')
+  get_replies('barackobama')
 
 
-@app.get("/")
+@app.get("/api/v1/accounts")
 def read_api(db: Session = Depends(get_db)):
-  return db.query(models.Replies).all()
+  return db.query(models.Accounts).all()
 
 
 uvicorn.run(app, port = 8080, host = "0.0.0.0")
