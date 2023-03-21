895c373ef8135a70aa3a744c03af96c6d32a6c78
Mohammad Hadiomid <hadiomidm@gmail.com>
2023-03-21 15:53:33 +0000

    Create ac_description3 function in order to get account summary and store it in database

--- scrapers.py
+++ scrapers.py
@@ -5,6 +5,48 @@ from sqlalchemy.orm import Session
 from sentiment import sentiment_analyzer
 
 
+def ac_description3(username):
+  profile = sntwitter.TwitterUserScraper(username).entity
+  name = profile.displayname
+  if profile.renderedDescription == '':
+    desc = f'{name} writes no description to this account.'
+  else:
+    desc = f'The account description is: {profile.renderedDescription}.'
+  if profile.verified:
+    verified = ''
+  else:
+    verified = 'NOT '
+  created_at = profile.created.year
+  followers_count = profile.followersCount
+  friends_count = profile.friendsCount
+  if profile.location == '':
+    location = ''
+  else:
+    location = f'{name} lives in {profile.location}.'
+  tweets_count = profile.statusesCount
+  if profile.link:
+    weblink = f'To Know more about {name} click on:\n {profile.link.url}'
+  else:
+    weblink = ''
+  if profile.label:
+    label_desc = profile.label.description
+    label_url = profile.label.url
+    work_page = f'\nIt is interesting that {name} has a work page which is connected to this twitter page.\
+                click here to see the page. {label_desc}:\n {label_url}'
+  else:
+    work_page = ''
+  text = f'This Twitter account belongs to {name} who was joined to this socila media at {created_at} and \
+          has written {tweets_count} tweets.\n\
+          This Account is {verified}verified and has {followers_count} followers with {friends_count} persons \
+          following this account who might be {name} close friends. \n\
+          {location} \n\
+          {desc} \n\
+          {weblink}\n\
+          {work_page}'
+
+  return text
+
+
 def get_accounts(accounts_list:list):
   for account in accounts_list:
     profile = sntwitter.TwitterUserScraper(account).entity
@@ -32,6 +74,7 @@ def get_accounts(accounts_list:list):
                   weblink = weblink,
                   label_desc = label_desc,
                   label_url = label_url,
+                  summary_desc = ac_description3(account)
       )
       session.add(account_model)
       session.commit()
